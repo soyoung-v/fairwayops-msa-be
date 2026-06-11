3608f4330e8a07a826f58e85b5244478c929ed45
soyoung <ssssy93@icloud.com>
2026-06-12 02:08:59 +0900

    init: 프로젝트 초기 생성

---
+++ src/test/java/com/fairwayops/fairwayopsmsabe/FairwayOpsMsaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairwayops.fairwayopsmsabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairwayOpsMsaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
